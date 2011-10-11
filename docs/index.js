3e54997c7d61f06a5c2bee263a514f760182e211
Yiannis Chatzikonstantinou <yconst@gmail.com>
2011-10-11 12:27:00 +0200

    Added simple Run feature.

--- docs/index.js
+++ docs/index.js
@@ -4,5 +4,17 @@
         var jsCode = Processing.compile(processingCode).sourceCode;
         $('#output-field').val(jsCode);
         $('#output-div').slideDown(400);
-    })
+    });
+    $('#run-button').click(function() {
+        var processingCode = $('#input-field').val();
+        var jsCode = Processing.compile(processingCode).sourceCode;
+        var pjs = new Processing($('.run-canvas')[0], processingCode);
+        $('#run-div').slideDown(400);
+    });
+    $('#close-button').click(function() {
+        $('#run-div').slideUp(400, function() {
+            $('.run-canvas').remove();
+            $('#run-div').prepend('<canvas class="run-canvas"></canvas>');
+        });
+    });
 })(jQuery);
